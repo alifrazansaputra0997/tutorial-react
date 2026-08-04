ca026fb969cd913732caa8f2d793c77e415e5526
Alif Razan Saputra <alifrazan97@gmail.com>
2026-08-04 23:10:55 +0700

    43. Making Components Reusable

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+import componentImg from './assets/components.png';
 
-function getRandomInt(max){
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+function getRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
-};
+}
 
 function Header() {
-  const description =  reactDescriptions[getRandomInt(1)];
+  const description = reactDescriptions[getRandomInt(1)];
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        {description} Fundamental React concepts you will need for almost any app you are
-        going to build!
+        {description} Fundamental React concepts you will need for almost any
+        app you are going to build!
       </p>
     </header>
+  );
+}
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.description} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
   );
 }
 function App() {
@@ -22,7 +33,15 @@ function App() {
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept 
+              title="Components" 
+              description="The Core UI building Block." 
+              image={componentImg}/>
+          </ul>
+        </section>
       </main>
     </div>
   );
