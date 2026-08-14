ade801e120083b4110f2a7d86564958933f85817
Alif Razan Saputra <alifrazan97@gmail.com>
2026-08-14 23:24:44 +0700

    rendering content conditionally #1

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,32 @@
 import componentImg from "./assets/components.png";
 import { CORE_CONCEPTS } from "./data.js";
-import Header from './components/Header/Header';
-import CoreConcept from './components/CoreConcept/CoreConcept';
-import TabButton from './components/TabButton/TabButton';
-import { EXAMPLES } from './data.js';
+import Header from "./components/Header/Header";
+import CoreConcept from "./components/CoreConcept/CoreConcept";
+import TabButton from "./components/TabButton/TabButton";
+import { EXAMPLES } from "./data.js";
 
-import { useState } from 'react';
+import { useState } from "react";
 
 function App() {
-  
   // selectedTopic = current state value
-  // setSelectedTopic = a function that provide from react for update the current state value 
-  const [selectedTopic, setSelectedTopic] = useState('components'); // useState always return array!
+  // setSelectedTopic = a function that provide from react for update the current state value
+  const [selectedTopic, setSelectedTopic] = useState(""); // useState always return array!
 
-
-  function onSelect(param){
+  function onSelect(param) {
     setSelectedTopic(param);
-    
+  }
+
+  let tabContent = <p>Please select a topic.</p>
+  if(selectedTopic){
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
   }
 
   return (
@@ -31,37 +41,30 @@ function App() {
               description={CORE_CONCEPTS[0].description}
               image={CORE_CONCEPTS[0].image}
             />
-              <CoreConcept
+            <CoreConcept
               title={CORE_CONCEPTS[1].title}
               description={CORE_CONCEPTS[1].description}
               image={CORE_CONCEPTS[1].image}
             />
-              <CoreConcept
+            <CoreConcept
               title={CORE_CONCEPTS[2].title}
               description={CORE_CONCEPTS[2].description}
               image={CORE_CONCEPTS[2].image}
             />
-              <CoreConcept {...CORE_CONCEPTS[3]}
-            />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => onSelect('components')}>Components</TabButton>
-            <TabButton onSelect={() => onSelect('jsx')}>JSX</TabButton>
-            <TabButton onSelect={() => onSelect('props')}>Props</TabButton>
-            <TabButton onSelect={() => onSelect('state')}>State</TabButton>
+            <TabButton onSelect={() => onSelect("components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => onSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => onSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => onSelect("state")}>State</TabButton>
           </menu>
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>
-              {EXAMPLES[selectedTopic].code}
-              </code>
-            </pre>
-          </div>
+          {tabContent}
         </section>
       </main>
     </div>
