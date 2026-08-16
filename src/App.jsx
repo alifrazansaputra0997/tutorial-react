4d73165f472f5d2b717be36816578009d7a34c89
Alif Razan Saputra <alifrazan97@gmail.com>
2026-08-16 08:26:38 +0700

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,10 @@
-import { useState } from "react";
-
-import { CORE_CONCEPTS } from "./data.js";
-import Header from "./components/Header/Header.jsx";
-import CoreConcept from "./components/CoreConcept.jsx";
-import TabButton from "./components/TabButton.jsx";
-import { EXAMPLES } from "./data.js";
-
-import CoreConcepts from "./components/CoreConcepts.jsx";
-import Examples from "./components/Examples.jsx";
-
-import ButtonPage from "./practices/practice-15/ButtonPage.jsx";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState();
-
-  function handleSelect(selectedButton) {
-    setSelectedTopic(selectedButton);
-  }
-
-  let tabContent = <p>Please select a topic.</p>;
-
-  if (selectedTopic) {
-    tabContent = (
-      <div id="tab-content">
-        <h3>{EXAMPLES[selectedTopic].title}</h3>
-        <p>{EXAMPLES[selectedTopic].description}</p>
-        <pre>
-          <code>{EXAMPLES[selectedTopic].code}</code>
-        </pre>
-      </div>
-    );
-  }
+  
 
   return (
-    <>
-      <ButtonPage />
-    </>
-    // <div>
-    //   <Header />
-    //   <main>
-    //     <CoreConcepts CORE_CONCEPTS={CORE_CONCEPTS} />
-    //     <Examples
-    //       onSelect={(param) => handleSelect(param)}
-    //       selectedTopic={selectedTopic}
-    //       tabContent={tabContent}
-    //     />
-    //   </main>
-    // </div>
-  );
+    <h1>React Tic-Tac-Toe</h1>
+  )
 }
 
-export default App;
+export default App
--- src/components/Tabs.jsx
+++
@@ -1,9 +0,0 @@
-import TabButton from "./TabButton.jsx";
-export default function Tabs({ children, buttons, ButtonsContainer = 'menu'}) {
-  return (
-    <>
-      <ButtonsContainer>{buttons}</ButtonsContainer>
-      {children}
-    </>
-  );
-}
